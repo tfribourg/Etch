d1176584b3439dca84754b6376b2643c97817ce1
tfribourg <103950978+tfribourg@users.noreply.github.com>
2022-08-01 16:10:12 -0400

    Alt JS not using display:grid

--- java.js
+++ java.js
@@ -1,38 +1,21 @@
 const container = document.querySelector('.container')
+const sizeOf = 16
 
-const div1 = document.createElement('div')
-container.appendChild(div1)
-const div2 = document.createElement('div')
-container.appendChild(div2)
-const div3 = document.createElement('div')
-container.appendChild(div3)
-const div4 = document.createElement('div')
-container.appendChild(div4)
-const div5 = document.createElement('div')
-container.appendChild(div5)
-const div6 = document.createElement('div')
-container.appendChild(div6)
-const div7 = document.createElement('div')
-container.appendChild(div7)
-const div8 = document.createElement('div')
-container.appendChild(div8)
-const div9 = document.createElement('div')
-container.appendChild(div9)
-const div10 = document.createElement('div')
-container.appendChild(div10)
-const div11 = document.createElement('div')
-container.appendChild(div11)
-const div12 = document.createElement('div')
-container.appendChild(div12)
-const div13 = document.createElement('div')
-container.appendChild(div13)
-const div14 = document.createElement('div')
-container.appendChild(div14)
-const div15 = document.createElement('div')
-container.appendChild(div15)
-const div16 = document.createElement('div')
-container.appendChild(div16)
+// create div function
+const createGrid = (amtGrids) =>{
+    for (let i = 0; i < amtGrids; i++ ){
+        const row = document.createElement('div')
+        row.classList.add('grid-row')
 
-
-
+     for (let j=0; j < amtGrids; j++){ 
+        const WandH = 960 / sizeOf
+        const gridBox = document.createElement('div')
+        gridBox.classList.add('grid-box')
+        gridBox.style.width
+        row.appendChild(gridBox)
+    }
+    container.appendChild(row)
+}
+}
+createGrid(sizeOf)
 
